0e01973621d0d205e537373ab8730c6c9b1468f2
ldt <lam@MacBook-Pro.local>
2024-08-29 07:53:31 +0700

    fix

--- popup.js
+++ popup.js
@@ -1,20 +1,104 @@
-// A minimal popup script for a manifest v3 Chrome extension.
-// Add your popup logic here.
-
 document.addEventListener("DOMContentLoaded", function () {
-  const alertButton = document.getElementById("alertButton");
+  document
+    .getElementById("tsvFile")
+    .addEventListener("change", function (event) {
+      let file = event.target.files[0];
+      if (!file) {
+        return;
+      }
+      let reader = new FileReader();
+      reader.onload = function (e) {
+        let [headers, ...rows] = e.target.result
+          .trim()
+          .split("\n")
+          .map((row) => row.split("\t"));
 
-  alertButton.addEventListener("click", () => {
+        const uploadPack = document.querySelector("#uploadPack");
+        let dataSource = rows.map((row) => row[headers.length - 1].trim());
+        dataSource = [...new Set(dataSource)];
+
+        dataSource.forEach((value) => {
+          const group = document.createElement("div");
+          group.classList.add("group-upload");
+          group.setAttribute("target", value);
+
+          const title = document.createElement("h4");
+          title.textContent = `Nhóm file ${value}`;
+          group.appendChild(title);
+
+          for (let i = 0; i < 4; i++) {
+            const input = document.createElement("input");
+            switch (i) {
+              case 0:
+                input.setAttribute("name", "urlTaiLieuKyThuat");
+                break;
+              case 1:
+                input.setAttribute("name", "urlTaiLieuHuongDan");
+                break;
+              case 2:
+                input.setAttribute("name", "fileTaiLieuHuongDanVn");
+                break;
+              case 3:
+                input.setAttribute("name", "fileMauNhan");
+                break;
+            }
+            input.type = "file";
+            group.appendChild(input);
+          }
+          uploadPack.appendChild(group);
+        });
+
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+          chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id },
+            func: test,
+            args: [dataSource],
+          });
+        });
+      };
+      reader.readAsText(file);
+    });
+
+  document.getElementById("startBtn").addEventListener("click", () => {
+    const groupFile = document.getElementsByClassName("group-upload");
+
+    const fileArray = Array.from(groupFile).map((group) => {
+      const fileObject = {};
+      Array.from(group.getElementsByTagName("input")).forEach((input) => {
+        if (input.type === "file" && input.files.length) {
+          fileObject[input.name] = input.files;
+        }
+      });
+      return fileObject;
+    });
+
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       chrome.scripting.executeScript({
         target: { tabId: tabs[0].id },
-        func: changeContent,
+        func: test,
+        args: [fileArray],
       });
     });
   });
 });
 
-function changeContent() {
+function test(data) {
+  console.log(data);
+}
+
+function changeContent(csv) {
+  const [headers, ...rows] = csv
+    .trim()
+    .split("\n")
+    .map((row) => row.split("\t"));
+
+  const dataSource = rows.map((row) =>
+    Object.fromEntries(
+      row.map((value, i) => [headers[i]?.trim(), value?.trim()])
+    )
+  );
+
+  var listCountries = [];
   let _addMoreBtn = $(
     'input[type=button][value="Thêm chủng loại/mã sản phẩm"]'
   );
@@ -51,37 +135,61 @@ function changeContent() {
   let _factoryCountry = (index) => {
     return _form().find(`[id$='_dsCoSoSx.nuocsx_${index}']`);
   };
-  let _updateCountry = (index, name) => {
-    _factoryCountry().find("option");
-  };
   let _isLoaded = () => {
-    console.log(111, _form().find(".loadingmask-message:visible").length);
     return !_form().find(".loadingmask-message:visible").length;
   };
 
-  const waitUntil = (f) => {
-    return new Promise((resolve) => {
-      const intervalId = setInterval(() => {
-        if (f()) {
-          clearInterval(intervalId);
-          resolve();
-        }
-      }, 1000);
+  async function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  async function waitUntil(check, interval = 1000) {
+    await sleep(interval);
+    do {
+      await new Promise((resolve) => setTimeout(resolve, interval));
+    } while (!(await check()));
+  }
+
+  function fillData(item) {
+    console.log(item);
+
+    _deviceName().val(item.deviceName);
+    _saleName().val(item.saleName);
+    _quality().val(item.quality);
+    _type().val(item.type);
+    _pack().val(item.pack);
+    _code().val(item.code);
+    _addFactoryBtn().click();
+
+    if (!listCountries.length) {
+      listCountries = _form()
+        .find(`[id$='_dsCoSoSx.nuocsx_0'] option`)
+        .toArray()
+        .reduce((acc, el) => {
+          acc[$(el).text().trim()] = $(el).val();
+          return acc;
+        }, {});
+    }
+    console.log(listCountries);
+
+    _factoryName(0).val(item.factoryName);
+    _factoryAddress(0).val(item.factoryAddress);
+    _factoryCountry(0).val(listCountries[item.factoryCountry]);
+  }
+
+  const startCode = async (f) => {
+    _addMoreBtn.click();
+
+    await waitUntil(_isLoaded);
+
+    fillData(dataSource[0]);
+    return true;
+    dataSource.forEach(async (item) => {
+      _addMoreBtn.click();
+      await waitUntil(_isLoaded);
+      fillData(item);
     });
   };
 
-  function test() {
-    _deviceName().val("bơm tiêm trâu");
-    _saleName().val("bơm tiêm gà");
-    _quality().val("Tiêu chuẩn của NSX");
-    _type().val("Chủng loại 1");
-    _pack().val("30 cái/hộp");
-    _code().val("ldt1234");
-    _addFactoryBtn().click();
-    _factoryName(0).val("Cơ sở 1");
-    _factoryAddress(0).val("Số 123");
-  }
-
-  _addMoreBtn.click();
-  waitUntil(_isLoaded).then(() => test());
+  startCode();
 }
